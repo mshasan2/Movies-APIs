a529063f834217ab746358efa03ac697c41572b5
mshasan2 <55490970+mshasan2@users.noreply.github.com>
2023-08-29 14:55:50 -0400

    Initial Commit

---
+++ src/main/java/com/example/movies/MoviesApplication.java
@@ -0,0 +1,26 @@
+package com.example.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication // This annotation is needed to make this class a Spring Boot application
+@RestController // The @RestController annotation tells Spring that this code describes an endpoint that should be made available over the web
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	// This method is a GET endpoint at the root of our API
+	// It returns a String with the text "Hello world!"
+	// The @GetMapping annotation maps HTTP GET requests onto specific handler methods
+	@GetMapping("/") // This annotation is needed to make this method a GET endpoint
+	public String apiRoot() {
+		return "Hello world!";
+	}
+
+
+
+}
